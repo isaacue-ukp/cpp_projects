e1b13a3be56f528df2992f505efa085e0b144bef
你çisaacue-ukp <sunruiqing247@gmail.com>
2026-08-19 11:17:10 +0800

    E03670

--- OJ/E03670CalculateTheSaddlePoint.cpp
+++ OJ/E03670CalculateTheSaddlePoint.cpp
@@ -6,34 +6,55 @@ int main()
     // 关闭同步，加速 cin/cout 输入输出
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
-    
-    //input
-    int matrix[5][5];
+
+    // input
+    int m[5][5];
     for (int i = 0; i < 5; ++i)
         for (int j = 0; j < 5; ++j)
-            cin >> matrix[i][j];
+            cin >> m[i][j];
+
     
-    //look for saddle point
-    int ROW[5], col[5];
+    // look for Minimun and Maxinum
+    bool RowMax[5][5] = {}, ColMin[5][5] = {};
     for (int i = 0; i < 5; ++i)
     {
-        ROW[i] = 0;
+        int temp = m[i][0];
+        for (int j = 1; j < 5; ++j)
+            if (temp < m[i][j])
+                temp = m[i][j];
         for (int j = 0; j < 5; ++j)
-            if (matrix[i][j] > matrix[i][ROW[i]])
-                ROW[i] = j;
+            if (temp == m[i][j])
+                RowMax[i][j] = 1;
     }
-
     for (int j = 0; j < 5; ++j)
     {
-        col[j] = 0;
+        int temp = m[0][j];
+        for (int i = 1; i < 5; ++i)
+            if (temp > m[i][j])
+                temp = m[i][j];
         for (int i = 0; i < 5; ++i)
-            if (matrix[i][j] > matrix[col[j]][j])
-                col[j] = i;
+            if (temp == m[i][j])
+                ColMin[i][j] = 1;
     }
 
+    //look for saddle point
+    bool SadPoi[5][5] = {};
+    bool Product = 0;
     for (int i = 0; i < 5; ++i)
-    {
         for (int j = 0; j < 5; ++j)
-    }
+        {
+            if (RowMax[i][j] && ColMin[i][j])
+                SadPoi[i][j] = 1;
+            Product += SadPoi[i][j];
+        }
+    
+    //print the result
+    if (!Product)
+        cout << "not found\n";
+    else
+        for (int i = 0; i < 5; ++i)
+            for (int j = 0; j < 5; ++j)
+                if (SadPoi[i][j])
+                    cout << i + 1 << " " << j + 1 << " " << m[i][j] << "\n";
     return 0;
 }
